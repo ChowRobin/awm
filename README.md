33351496834f904066594313d12805ee0eb5cb36
robin <robinchow8991@gmail.com>
2018-09-11 00:06:17 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# awm
+- a take-away api, provided to the user client and the merchant client data interface.
+- based on SpringBoot, Mybatis, mysql, redis, etc.
---
+++ src/main/java/pers/robin/awm/AwmApplication.java
@@ -0,0 +1,12 @@
+package pers.robin.awm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmApplication.class, args);
+    }
+}
